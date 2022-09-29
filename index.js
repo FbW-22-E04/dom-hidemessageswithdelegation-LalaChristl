6e524ce1bd36bfcc0ce551ecdfea3d323cb90b9c
LalaChristl <lesleyannchristl@gmail.com>
2022-09-29 19:01:30 +0200

    added soution

---
+++ index.js
@@ -0,0 +1,10 @@
+  const container = document.querySelector("#container");
+
+  container.addEventListener("click", (event) => {
+    if (event.target.className === "remove-button") {
+      let pane = event.target.closest(".pane");
+      pane.remove();
+    }
+  });
+
+  
